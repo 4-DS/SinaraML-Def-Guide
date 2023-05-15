f41bf476f04de08cc28caa03750de940ef45929e
Dmitry-Love <Dmitry.lyubushkin@gmail.com>
2023-05-15 17:57:40 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Sinara ML Definitive Guide'
+copyright = '2023, 4-DS'
+author = 'Dmitry Love'
 
 release = '0.1'
 version = '0.1.0'
