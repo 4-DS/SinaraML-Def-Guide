8f1f7a3896986fa9ce5f0cd0830f78eded587898
Melchin1a <55787399+Melchin1a@users.noreply.github.com>
2023-11-14 02:05:26 +0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,8 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.intersphinx',    
+    'sphinx_rtd_theme',
 ]
 
 intersphinx_mapping = {
